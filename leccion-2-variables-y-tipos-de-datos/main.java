5e1c8cb15030016ac61a37732c01e56887b70861
Samuel <samuelbonifacio015@gmail.com>
2025-11-15 14:07:28 -0500

    initial commit

---
+++ leccion-2-variables-y-tipos-de-datos/main.java
@@ -0,0 +1,17 @@
+/**
+ * Recuerda copiar el codigo en un entorno de replit
+ * O iniciar un proyecto vacío con IntelliJ IDEA Ultimate
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    String nombre = "Ana";
+    int edad = 15;
+    boolean paginaActiva = true;
+
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Edad: " + edad);
+    System.out.println("Pagina Activa: " + paginaActiva);
+  }
+
+}
